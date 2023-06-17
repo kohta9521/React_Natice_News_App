4bbeaf3911b0be80fed8fd7ea102d12824d80880
Kohta9521 <kohta0421kochi@gmail.com>
2023-06-17 21:45:21 +0900

    add news api and complete

--- news-app/App.js
+++ news-app/App.js
@@ -2,13 +2,22 @@ import { StatusBar } from "expo-status-bar";
 import { useState, useEffect } from "react";
 import { StyleSheet, SafeAreaView, FlatList } from "react-native";
 import { ListItem } from "./components/ListItem";
-import dummyArticles from "./dummies/articles.json";
+// import dummyArticles from "./dummies/articles.json";
+import axios from "axios";
+import Constants from "expo-constants";
 
 export default function App() {
   const [articles, setArticles] = useState([]);
 
-  const fetchArticles = () => {
-    setArticles(dummyArticles);
+  const URL =
+    "https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=ee6690c543b748fd95ec420e32abac49";
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+    } catch (err) {
+      console.error(error);
+    }
   };
 
   useEffect(() => {
